c73f662616115191988abe75f54364f2f6372b4b
Hidde van der Ploeg <hiddevanderploeg@me.com>
2023-09-02 12:47:51 +0100

    First project

---
+++ HeartRateTest/HeartRateTest Watch App/HeartRateTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  HeartRateTestApp.swift
+//  HeartRateTest Watch App
+//
+//  Created by Hidde van der Ploeg on 28/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct HeartRateTest_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
